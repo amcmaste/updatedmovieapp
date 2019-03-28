1437bef4d0e8bd11cee870f5dd6ecc6008ad26f5
Andrew <amcmaste@gmail.com>
2019-03-28 10:00:37 -0700

    Question population working

--- app/static/movie.js
+++ app/static/movie.js
@@ -37,8 +37,55 @@ $(document).ready(function() {
         url : '/movie'
       })
 	  .done(function(response) {
+		  
+		let quest = $('.questions-container');
+		
+		quest.html('');
 	    
-		alert(JSON.stringify(response));
+		for (let question in response) {
+		  quest.append(
+		  `
+            <div class="question-specific-container">
+              <div class="question-content-container">
+                <div class="question-points">POINTS</div>
+				<div class="question-voting">
+				  <i class="fas fa-arrow-alt-circle-up"></i>
+			      <i class="fas fa-arrow-alt-circle-down"></i>
+				</div>
+				<div class="question-content">CONTENT</div>
+              </div>
+              <div class="answers-container">
+              </div>
+              <div class="more-answers-container">
+                <button class="more-answers-button btn btn-primary more-button">More Answers</button>
+              </div>
+              <div class="add-answer-container">
+                {{  wtf.quick_form(answer, id="add-answer-form-contents")  }}
+              </div>
+            </div>
+		  `
+		  );
+		  
+		  let ans = $('.answers-container');
+		  
+		  for (let answer in question.answers) {
+			ans.append(
+            `
+              <div class="answer-specific-container">
+                <div class="answer-content-container">
+                  <div class="answer-points">POINTS</div>
+				  <div class="answer-voting">
+				    <i class="fas fa-arrow-alt-circle-up"></i>
+					<i class="fas fa-arrow-alt-circle-down"></i>
+			      </div>
+				  <div class="answer-content">CONTENT</div>
+                </div>
+              </div>
+			`
+			);			
+		  }
+		  
+		}
 	  
 	  })
 	
